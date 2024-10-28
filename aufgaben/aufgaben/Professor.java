0b25d34fe8b0eae28673fe4f3259ae042e156110
nouhaelq <nouhaa.2428@gmail.com>
2024-10-28 23:03:23 +0100

    1.Aufgabe

---
+++ aufgaben/aufgaben/Professor.java
@@ -0,0 +1,22 @@
+package aufgaben.aufgaben;
+
+public class Professor {
+  private String name;
+
+  public Professor(){
+
+  }
+  public Professor(String name){
+    this.name = name;
+  }
+
+  public void setName(String name){
+    this.name = name;
+  }
+
+  public String getName(){
+     return name;
+  }
+
+
+}
